aa1a1b12cc86098f9406a06030f53caa00d3e2d2
c0lares <mcolaresc@gmail.com>
2021-12-04 23:42:19 -0300

    desafios modulo 5 Final

--- quantificadores/desafioCpf.js
+++ quantificadores/desafioCpf.js
@@ -1,6 +1,12 @@
-const texto = `CPF dos aprovados:
-- 600.567.890-12
-- 765.998.345-23 ...`
+const texto = `
+CPF dos aprovados:
+  - 600.567.890-12
+  - 765.998.345-23
+  - 454.674.333-21
+  - 678.987.123-87
+  - 789.112.543-00
+`
+
 
 const regexCpf = /\d{3}.\d{3}.\d{3}-\d{2}/g
 console.log(texto.match(regexCpf))
--- quantificadores/desafioEmail.js
+++ quantificadores/desafioEmail.js
@@ -1,7 +1,17 @@
-const texto = `Os e-mails dos convidados são:
-- fulano@cod3r.com.br
-- xico@gmail.com ...`
+const texto =`
+Os e-mails dos convidados são:
+  - fulano@cod3r.com.br
+  - xico@gmail.com
+  - joao@empresa.info.br
+  - maria_silva@registro.br
+  - rafa.sampaio@yahoo.com
+`
 
-const regexEmail = /\w+@\w+\.\w+\.?\w+/g
 
-console.log(texto.match(regexEmail))
+const regexEmail = /[\w.]+@\w+\.\w+\.?\w+/g
+const regexEmail2 = /[\w.]+@\w+\.\w{2,4}/g
+
+console.log(texto.match(regexEmail2))
+
+// no futuro...
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}(\.\w{2})?/g))
